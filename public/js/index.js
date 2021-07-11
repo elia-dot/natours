45dd0893b6789ef3e73c87cdb4ca023c0d25f57a
elia-dot <eliacohen100@gmail.com>
2021-07-11 14:54:16 +0300

    bug fixed

--- public/js/index.js
+++ public/js/index.js
@@ -64,5 +64,5 @@ if (bookBtn)
     bookTour(tourId);
   });
 
-// const alertMessage = document.querySelector('body').dataset.alert;
-// if (alertMessage) showAlert('success', alertMessage, 20);
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
