178067b2199f5a7a6748e7d747b9ff110cedc112
elia-dot <eliacohen100@gmail.com>
2021-07-11 07:54:42 +0300

    implemented cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const csp = require('express-csp');
 const compression = require('compression');
+const cors = require('cors')
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,6 +25,9 @@ app.enable('trust proxy')
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(cors())
+app.options('*', cors())
 
 app.use(express.static(path.join(__dirname, 'public')));
 
