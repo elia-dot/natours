45506d11cafd2be82449afdfca9ce3d25542ac03
elia-dot <eliacohen100@gmail.com>
2021-07-11 08:53:16 +0300

    improved stripe

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.post('webhook-checkout', express.raw({type: 'application/json'}), webhookCheckout)
+app.post('/webhook-checkout', express.raw({type: 'application/json'}), webhookCheckout)
 
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
